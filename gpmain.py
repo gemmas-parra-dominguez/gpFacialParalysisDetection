a328ec7e7e59616ba4e1eaafd9f86f2a34bab8eb
Gemma S. Parra-Dominguez <gemmasparra@gmail.com>
2026-05-25 12:30:20 -0600

    Added code improvements after PR

--- gpmain.py
+++ gpmain.py
@@ -32,7 +32,6 @@ def gpFaceReg(image_face, data_keypts):
 
     return rot_face, mat_landmarks
 
-
 def gpPtsExt(mat_landmarks):
     coord_vector = np.zeros((TOTAL_LANDMARK, 2), dtype=np.float32)
     cont = 0
@@ -107,28 +106,39 @@ def gpGetFMM(norm_vector, face_smr_data):
     mr_dist += gpEuclideanDist(norm_vector[36, 0], norm_vector[36, 1], norm_vector[37, 0], norm_vector[37, 1])
     mr_dist += gpEuclideanDist(norm_vector[37, 0], norm_vector[37, 1], norm_vector[31, 0], norm_vector[31, 1])
 
-    if b_dist > c_dist: face_smr_data[18, 0] = b_dist / a_dist
-    else: face_smr_data[18, 0] = c_dist / a_dist
+    if b_dist > c_dist:
+        face_smr_data[18] = b_dist / a_dist
+    else:
+        face_smr_data[18] = c_dist / a_dist
 
-    if sl_dist > tl_dist: face_smr_data[19, 0] = sl_dist / h_dist
-    else: face_smr_data[19, 0] = tl_dist / h_dist
+    if sl_dist > tl_dist:
+        face_smr_data[19] = sl_dist / h_dist
+    else:
+        face_smr_data[19] = tl_dist / h_dist
 
-    if su_dist > tu_dist: face_smr_data[20, 0] = su_dist / h_dist
-    else: face_smr_data[20, 0] = tu_dist / h_dist
+    if su_dist > tu_dist:
+        face_smr_data[20] = su_dist / h_dist
+    else:
+        face_smr_data[20] = tu_dist / h_dist
 
-    if ml_dist > mr_dist: face_smr_data[21, 0] = ml_dist / h_dist
-    else: face_smr_data[21, 0] = mr_dist / h_dist
+    if ml_dist > mr_dist:
+        face_smr_data[21] = ml_dist / h_dist
+    else:
+        face_smr_data[21] = mr_dist / h_dist
 
-    if d_dist > g_dist: face_smr_data[25, 0] = d_dist / a_dist
-    else: face_smr_data[25, 0] = g_dist / a_dist
+    if d_dist > g_dist:
+        face_smr_data[25] = d_dist / a_dist
+    else:
+        face_smr_data[25] = g_dist / a_dist
 
-    if e_dist > f_dist: face_smr_data[26, 0] = e_dist / a_dist
-    else: face_smr_data[26, 0] = f_dist / a_dist
+    if e_dist > f_dist:
+        face_smr_data[26] = e_dist / a_dist
+    else:
+        face_smr_data[26] = f_dist / a_dist
 
-    face_smr_data[28, 0] = i_dist / a_dist
+    face_smr_data[28] = i_dist / a_dist
 
 def gpGetSMR(norm_vector, face_smr_data):
-    mineval = 0.0001
     b_dist = gpEuclideanDist(norm_vector[49, 0], norm_vector[49, 1], norm_vector[48, 0], norm_vector[48, 1])
     e_dist = gpEuclideanDist(norm_vector[37, 0], norm_vector[37, 1], norm_vector[10, 0], norm_vector[10, 1])
     f_dist = gpEuclideanDist(norm_vector[19, 0], norm_vector[19, 1], norm_vector[37, 0], norm_vector[37, 1])
@@ -145,12 +155,12 @@ def gpGetSMR(norm_vector, face_smr_data):
     ql_dist = gpEuclideanDist(norm_vector[11, 0], norm_vector[11, 1], norm_vector[15, 0], norm_vector[15, 1])
     qr_dist = gpEuclideanDist(norm_vector[12, 0], norm_vector[12, 1], norm_vector[14, 0], norm_vector[14, 1])
     q_avg = (abs(norm_vector[11, 1] - norm_vector[15, 1]) + abs(norm_vector[12, 1] - norm_vector[14, 1])) / 2.0
-    if q_avg <= mineval: q_avg = 0
+    if q_avg <= MIN_EVAL: q_avg = 0
 
     rl_dist = gpEuclideanDist(norm_vector[17, 0], norm_vector[17, 1], norm_vector[21, 0], norm_vector[21, 1])
     rr_dist = gpEuclideanDist(norm_vector[18, 0], norm_vector[18, 1], norm_vector[20, 0], norm_vector[20, 1])
     r_avg = (abs(norm_vector[17, 1] - norm_vector[21, 1]) + abs(norm_vector[18, 1] - norm_vector[20, 1])) / 2.0
-    if r_avg <= mineval: r_avg = 0
+    if r_avg <= MIN_EVAL: r_avg = 0
 
     sl_dist = gpEuclideanDist(norm_vector[29, 0], norm_vector[29, 1], norm_vector[39, 0], norm_vector[39, 1])
     su_dist = gpEuclideanDist(norm_vector[30, 0], norm_vector[30, 1], norm_vector[38, 0], norm_vector[38, 1])
@@ -168,77 +178,108 @@ def gpGetSMR(norm_vector, face_smr_data):
 
     delta_x = norm_vector[0, 0] - norm_vector[9, 0]
     delta_y = norm_vector[0, 1] - norm_vector[9, 1]
-    if not math.isnan(math.atan2(delta_y, delta_x)): face_smr_data[0, 0] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
+    if not math.isnan(math.atan2(delta_y, delta_x)):
+        face_smr_data[0] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
 
     delta_x = norm_vector[2, 0] - norm_vector[7, 0]
     delta_y = norm_vector[2, 1] - norm_vector[7, 1]
-    if not math.isnan(math.atan2(delta_y, delta_x)): face_smr_data[1, 0] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
+    if not math.isnan(math.atan2(delta_y, delta_x)):
+        face_smr_data[1] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
 
     delta_x = norm_vector[4, 0] - norm_vector[5, 0]
     delta_y = norm_vector[4, 1] - norm_vector[5, 1]
-    if not math.isnan(math.atan2(delta_y, delta_x)): face_smr_data[2, 0] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
+    if not math.isnan(math.atan2(delta_y, delta_x)):
+        face_smr_data[2] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
 
-    if a_dist < i_dist: face_smr_data[3, 0] = a_dist / i_dist
-    else: face_smr_data[3, 0] = i_dist / a_dist
+    if a_dist < i_dist:
+        face_smr_data[3] = a_dist / i_dist
+    else:
+        face_smr_data[3] = i_dist / a_dist
 
-    face_smr_data[4, 0] = c_dist
-    face_smr_data[5, 0] = d_dist
-    face_smr_data[6, 0] = h_dist
+    face_smr_data[4] = c_dist
+    face_smr_data[5] = d_dist
+    face_smr_data[6] = h_dist
 
     delta_x = norm_vector[19, 0] - norm_vector[10, 0]
     delta_y = norm_vector[19, 1] - norm_vector[10, 1]
-    if not math.isnan(math.atan2(delta_y, delta_x)): face_smr_data[7, 0] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
+    if not math.isnan(math.atan2(delta_y, delta_x)):
+        face_smr_data[7] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
 
-    if gl_dist < gr_dist: face_smr_data[8, 0] = gl_dist / gr_dist
-    else: face_smr_data[8, 0] = gr_dist / gl_dist
+    if gl_dist < gr_dist:
+        face_smr_data[8] = gl_dist / gr_dist
+    else:
+        face_smr_data[8] = gr_dist / gl_dist
 
-    if j_dist < k_dist: face_smr_data[9, 0] = j_dist / k_dist
-    else: face_smr_data[9, 0] = k_dist / j_dist
+    if j_dist < k_dist:
+        face_smr_data[9] = j_dist / k_dist
+    else:
+        face_smr_data[9] = k_dist / j_dist
 
-    if m_dist < n_dist: face_smr_data[10, 0] = m_dist / n_dist
-    else: face_smr_data[10, 0] = n_dist / m_dist
+    if m_dist < n_dist:
+        face_smr_data[10] = m_dist / n_dist
+    else:
+        face_smr_data[10] = n_dist / m_dist
 
-    if q_avg < r_avg and r_avg != 0: face_smr_data[11, 0] = q_avg / r_avg
-    elif q_avg != 0: face_smr_data[11, 0] = r_avg / q_avg
-    else: face_smr_data[11, 0] = 0
+    if q_avg < r_avg and r_avg != 0:
+        face_smr_data[11] = q_avg / r_avg
+    elif q_avg != 0:
+        face_smr_data[11] = r_avg / q_avg
+    else:
+        face_smr_data[11] = 0
 
-    if ql_dist < rr_dist: face_smr_data[12, 0] = ql_dist / rr_dist
-    else: face_smr_data[12, 0] = rr_dist / ql_dist
+    if ql_dist < rr_dist:
+        face_smr_data[12] = ql_dist / rr_dist
+    else:
+        face_smr_data[12] = rr_dist / ql_dist
 
-    if qr_dist < rl_dist: face_smr_data[13, 0] = qr_dist / rl_dist
-    else: face_smr_data[13, 0] = rl_dist / qr_dist
+    if qr_dist < rl_dist:
+        face_smr_data[13] = qr_dist / rl_dist
+    else:
+        face_smr_data[13] = rl_dist / qr_dist
 
     delta_x = norm_vector[34, 0] - norm_vector[28, 0]
     delta_y = norm_vector[34, 1] - norm_vector[28, 1]
-    if not math.isnan(math.atan2(delta_y, delta_x)): face_smr_data[14, 0] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
+    if not math.isnan(math.atan2(delta_y, delta_x)):
+        face_smr_data[14] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
 
-    if e_dist < f_dist: face_smr_data[15, 0] = e_dist / f_dist
-    else: face_smr_data[15, 0] = f_dist / e_dist
+    if e_dist < f_dist:
+        face_smr_data[15] = e_dist / f_dist
+    else:
+        face_smr_data[15] = f_dist / e_dist
 
-    if sl_dist < tl_dist: face_smr_data[16, 0] = sl_dist / tl_dist
-    else: face_smr_data[16, 0] = tl_dist / sl_dist
+    if sl_dist < tl_dist:
+        face_smr_data[16] = sl_dist / tl_dist
+    else:
+        face_smr_data[16] = tl_dist / sl_dist
 
-    if su_dist < tu_dist: face_smr_data[17, 0] = su_dist / tu_dist
-    else: face_smr_data[17, 0] = tu_dist / su_dist
+    if su_dist < tu_dist:
+        face_smr_data[17] = su_dist / tu_dist
+    else:
+        face_smr_data[17] = tu_dist / su_dist
 
     delta_x = norm_vector[27, 0] - norm_vector[23, 0]
     delta_y = norm_vector[27, 1] - norm_vector[23, 1]
-    if not math.isnan(math.atan2(delta_y, delta_x)): face_smr_data[22, 0] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
+    if not math.isnan(math.atan2(delta_y, delta_x)):
+        face_smr_data[22] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
 
     delta_x = norm_vector[37, 0] - norm_vector[22, 0]
     delta_y = norm_vector[37, 1] - norm_vector[22, 1]
-    if not math.isnan(math.atan2(delta_y, delta_x)): face_smr_data[23, 0] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
+    if not math.isnan(math.atan2(delta_y, delta_x)):
+        face_smr_data[23] = abs(math.atan2(delta_y, delta_x) * 180 / math.pi)
 
-    if o_dist < p_dist: face_smr_data[24, 0] = o_dist / p_dist
-    else: face_smr_data[24, 0] = p_dist / o_dist
+    if o_dist < p_dist:
+        face_smr_data[24] = o_dist / p_dist
+    else:
+        face_smr_data[24] = p_dist / o_dist
 
-    if b_dist != 0: face_smr_data[27, 0] = l_dist / b_dist
+    if b_dist != 0:
+        face_smr_data[27] = l_dist / b_dist
 
-def gpComputeSRM(num_test_images, test_image_root, init_img):
+def gpComputeSRM(num_test_images, test_image_root):
     no_img_found = 0
     face_found = 0
 
-    for ik in range(init_img, num_test_images):
+    for ik in range(num_test_images):
         if ik < 1000: cbuffs = f"{ik:03d}"
         else: cbuffs = f"{ik:04d}"
 
@@ -253,48 +294,43 @@ def gpComputeSRM(num_test_images, test_image_root, init_img):
             no_img_found += 1
             print("no data found...")
             smr_vector = np.array([[-1.0]])
-            file_name_out = test_results_path + "smfeat_" + "test_image_" + cbuffs + csv_name
+            file_name_out = test_results_path + sm_feat + image_root + cbuffs + csv_name
             writeCSV(file_name_out, smr_vector)
         else:
             img_lbf, mat_landmarks = gpFaceReg(img_color, data_keypts)
             for jk in range(FIRST_LANDMARK, data_keypts.shape[0]):
                 cv2.circle(img_lbf, (int(mat_landmarks[jk, 0]), int(mat_landmarks[jk, 1])), 6, (0, 0, 255), -1)
 
-            file_name_out = test_results_path + "test_image_" + cbuffs + "_keypts_out" + image_ext
+            file_name_out = test_results_path + image_root + cbuffs + "_facereg_keypts_out" + image_ext
             cv2.imwrite(file_name_out, img_lbf)
             face_found += 1
 
             coord_vector = gpPtsExt(mat_landmarks)
-            smr_vector = np.zeros((TOTAL_SMR, 1), dtype=np.float32)
+            smr_vector = np.zeros(TOTAL_SMR, dtype=np.float32)
             gpGetSMR(coord_vector, smr_vector)
             gpGetFMM(coord_vector, smr_vector)
 
-            file_name_out = test_results_path + "smfeat_" + "test_image_" + cbuffs + csv_name
+            file_name_out = test_results_path + sm_feat + image_root + cbuffs + csv_name
             writeCSV(file_name_out, smr_vector)
 
+    print(f"Number of images: {num_test_images}")
     print(f"Number of files not found: {no_img_found}")
     print(f"Number of files found: {face_found}")
-    print(f"Total of supposed data: {num_test_images - init_img}")
 
 if __name__ == '__main__':
-    # Equivalent to C++ main
-    test_image_path = os.getcwd() + "/test_data/"
-    test_results_path = os.getcwd() + "/test_results/"
+    # Setting the paths
+    test_image_path = os.getcwd() + os.path.sep + test_image_path + os.path.sep
+    test_results_path = os.getcwd() + os.path.sep + test_results_path + os.path.sep
+    test_image_root = test_image_path + image_root
 
-    test_image_root = test_image_path + "test_image_"
-
-    # Read the number of *.jpg files in test_image_root
-    jpg_files = glob.glob(test_image_path + "*.jpg")
+    # Read the number of files in test_image_root
+    jpg_files = glob.glob(test_image_path + '*' + image_ext)
     num_subjects = len(jpg_files)
 
     print(f"Processing images from {test_image_root}")
-    gpComputeSRM(num_subjects, test_image_root, 0)
+    gpComputeSRM(num_subjects, test_image_root)
 
-    # Equivalent to RUN.m
-    path = os.getcwd() + '/test_results/'
-    vec_labels = [[0], [1], [0], [0], [1]] # Example labels for the 5 subjects
-    cvs_name_dataset = 'face_aligment_dataset.csv'
-    arff_name_dataset = 'data_face-alignment.arff'
+    vec_labels = [[0], [1], [0], [0], [1]] # Example labels for the 5 subjects    
     print(f"Preparing ARFF dataset...")
-    gp_data_prepare_to_arff(path, vec_labels, cvs_name_dataset, arff_name_dataset)
+    gp_data_prepare_to_arff(test_results_path, vec_labels, cvs_name_dataset, arff_name_dataset)
     print("Done.")
